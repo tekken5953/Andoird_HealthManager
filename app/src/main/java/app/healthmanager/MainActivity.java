2ebc3ccc408bc7ba21e3c24b1f0bbf7e00858f45
tekken5953@naver.com <wodud1011>
2020-09-15 14:15:31 +0900

    commit

--- app/src/main/java/app/healthmanager/MainActivity.java
+++ app/src/main/java/app/healthmanager/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -21,17 +25,16 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import static android.view.View.GONE;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton btn1, btn2, btn3, btn4;
-    WebView webView;
-    TextView foldtx, appname;
-    Button webView_foodbtn, mask, medicien;
+    TextView appname;
+    Button webView_foodbtn, mask, medicien, search;
     LinearLayout titlelinear;
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,84 +47,41 @@ public class MainActivity extends AppCompatActivity {
         btn2 = (ImageButton) findViewById(R.id.btndetail);
         btn3 = (ImageButton) findViewById(R.id.btncalendar);
         btn4 = (ImageButton) findViewById(R.id.btnoption);
-        mask = (Button) findViewById(R.id.mask);
-        medicien = (Button) findViewById(R.id.medicien);
-        webView = (WebView) findViewById(R.id.webView);
+        mask = (Button) findViewById(R.id.webView_mask);
+        medicien = (Button) findViewById(R.id.webView_medicien);
         appname = (TextView) findViewById(R.id.appname);
         titlelinear = (LinearLayout) findViewById(R.id.titlelinear);
         webView_foodbtn = (Button) findViewById(R.id.webView_food_btn);
-        foldtx = (TextView) findViewById(R.id.foldingtx);
-        WebSettings webSettings = webView.getSettings();
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치기반 서비스 동의
-        webSettings.setAppCacheEnabled(false); // 캐시파일 경로 저장
-        webSettings.setJavaScriptEnabled(true); // 자바스크립트 허용
-        webSettings.setLoadWithOverviewMode(true); // 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
-        webView.setWebViewClient(new WebViewClient()); // 새창 뜨지 않게 하기위해서
+        search = (Button) findViewById(R.id.webView_search);
 
-        //페이지 로드 완료 후
-        webView.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return false;
-            }
-        });
-        foldtx.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ObjectAnimator animation = ObjectAnimator.ofFloat(titlelinear, "translationY", -650, 0);
-                animation.setDuration(700);
-                animation.start();
-                webView.setVisibility(GONE);
-                foldtx.setVisibility(GONE);
-            }
-        });
-        //마스크&약국 검색
         mask.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                ObjectAnimator animation = ObjectAnimator.ofFloat(titlelinear, "translationY", 0, -650);
-                animation.setDuration(700);
-                animation.start();
-                foldtx.setText("-- > 지도 접기 < --");
-                webView.setVisibility(View.VISIBLE);
-                foldtx.setVisibility(View.VISIBLE);
-                webView.loadUrl("https://m.map.kakao.com/actions/searchView?q=공적마스크판매처&viewmap=true");
+            public void onClick(View view) {
+                alertdialog("https://m.map.kakao.com/actions/searchView?q=공적마스크판매처&viewmap=true");
             }
         });
+
         medicien.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                ObjectAnimator animation = ObjectAnimator.ofFloat(titlelinear, "translationY", 0, -650);
-                animation.setDuration(700);
-                animation.start();
-                foldtx.setText("-- > 지도 접기 < --");
-                webView.setVisibility(View.VISIBLE);
-                foldtx.setVisibility(View.VISIBLE);
-                webView.loadUrl("https://m.map.kakao.com/actions/searchView?q=약국&viewmap=true");
+            public void onClick(View view) {
+                alertdialog("https://m.map.kakao.com/actions/searchView?q=약국&viewmap=true");
             }
         });
 
         webView_foodbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                    ObjectAnimator animation = ObjectAnimator.ofFloat(titlelinear, "translationY", 0,-650);
-                    animation.setDuration(700);
-                    animation.start();
-
-                foldtx.setText("-- > 식단 접기 < --");
-                webView.setVisibility(View.VISIBLE);
-                foldtx.setVisibility(View.VISIBLE);
-                webView.loadUrl("http://api.nongsaro.go.kr/sample/ajax/recomendDiet/recomendDietList.html");
+            public void onClick(View view) {
+                alertdialog("http://api.nongsaro.go.kr/sample/ajax/recomendDiet/recomendDietList.html");
             }
         });
 
-        //webview 에서 GPS location 권한을 사용하도록 추가
-        webView.setWebChromeClient(new WebChromeClient() {
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-                super.onGeolocationPermissionsShowPrompt(origin, callback);
-                callback.invoke(origin, true, false);
+            public void onClick(View view) {
+                alertdialog("http://dikmobile.health.kr/main.do");
             }
         });
+
 
         //Bottom Navegation Menu Click Event
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -173,5 +133,59 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .setNegativeButton("아니오", null)
                 .show();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public void alertdialog(String s) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View v = LayoutInflater.from(this).inflate(R.layout.webview,null, false);
+        builder.setView(v);
+        final AlertDialog alertDialog = builder.create();
+
+        Objects.requireNonNull(alertDialog.getWindow()).getAttributes().windowAnimations = R.style.AnimationPopupStyle;
+
+        Window window = alertDialog.getWindow();
+
+        // 백그라운드 투명
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams params = window.getAttributes();
+        // 화면에 가득 차도록
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+        // 열기&닫기 시 애니메이션 설정
+        params.windowAnimations = R.style.AnimationPopupStyle;
+        window.setAttributes(params);
+        // UI 정렬
+        window.setGravity(Gravity.CENTER);
+
+        final WebView webView = (WebView) v.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치기반 서비스 동의
+        webSettings.setAppCacheEnabled(false); // 캐시파일 경로 저장
+        webSettings.setJavaScriptEnabled(true); // 자바스크립트 허용
+        webSettings.setLoadWithOverviewMode(true); // 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
+        webView.setWebViewClient(new WebViewClient()); // 새창 뜨지 않게 하기위해서
+
+        //페이지 로드 완료 후
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        //webview 에서 GPS location 권한을 사용하도록 추가
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        webView.loadUrl(s);
+
+        alertDialog.show();
     }
 }
